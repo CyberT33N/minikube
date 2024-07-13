5ef8dcf622c4131e6f5d684bacff1f03ca42ee48
CyberT33N <checkGitHubForEmail@gmail.com>
2024-07-13 15:44:02 +0200

    feat(CCS-001): added mongodb helm chart

--- README.md
+++ README.md
@@ -1,1 +1,114 @@
-# minikube
+# Minikube
+
+
+
+
+
+<br><br>
+<br><br>
+_____________________________________
+_____________________________________
+<br><br>
+<br><br>
+
+## IP
+```shell
+minikube ip
+```
+- Should be 192.168.49.2
+
+
+
+
+
+
+
+
+
+<br><br>
+<br><br>
+_____________________________________
+_____________________________________
+<br><br>
+<br><br>
+
+
+# Services
+```shell
+kubectl get svc --namespace=dev
+```
+
+
+
+
+
+<br><br>
+<br><br>
+_____________________________________
+_____________________________________
+<br><br>
+<br><br>
+
+# Helm Charts
+
+<br><br>
+<br><br>
+
+## MongoDB
+
+### Connection String
+- `mongodb://root:test@192.168.49.2.nip.io:30644/`
+
+<br><br>
+<br><br>
+
+### Add repo
+```shell
+# Add bitnami repo
+helm repo add bitnami https://charts.bitnami.com/bitnami
+
+# Update helm repo
+helm repo update
+
+# Auflisten der verfügbaren Helm Chart Versionen
+helm search repo bitnami/mongodb --versions
+```
+
+<br><br>
+<br><br>
+
+### Install Helm Chart
+```shell
+# This will download the mongodb helm chart to the folder ./mongodb/Chart
+cd ~/Projects/minikube
+mkdir -p ./mongodb/Chart
+
+# 15.6.12 = MongoDB 7
+helm pull bitnami/mongodb --version 15.6.12 --untar --untardir ./tmp
+cp -r ./tmp/mongodb/* ./mongodb/Chart
+rm -rf ./tmp
+
+# Create custom-values.yaml
+touch ./mongodb/custom-values.yaml
+
+# Change context
+kubectl config use-context minikube
+
+# Install
+helm install mongodb-dev ./mongodb/Chart --namespace dev -f ./mongodb/custom-values.yaml
+```
+
+<br><br>
+<br><br>
+
+### Upgrade Helm Chart
+```shell
+kubectl config use-context minikube
+helm upgrade mongodb-dev ./mongodb/Chart --namespace dev -f ./mongodb/custom-values.yaml --atomic
+```
+
+### Delete Deployment
+```shell
+kubectl config use-context minikube
+helm --namespace dev delete mongodb-dev
+```
---
+++ start.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -e
+
+# Prevent "Exiting due to PROVIDER_DOCKER_NEWGRP - dial unix
+# /var/run/docker.sock: connect: permission denied"
+# sudo usermod -aG docker $USER && newgrp docker
+
+_directory=$(dirname $0)
+
+minikube start \
+   --cpus=8 \
+   --memory=18G \
+   --driver=docker \
+   --addons storage-provisioner \
+   --addons inaccel \
+   --addons kubeflow \
+   --addons logviewer
+
+echo "minikube start done.."
+
+MINIKUBE_IP=$(minikube ip)
+
+echo "Minikube IP: $MINIKUBE_IP"
+
+FROM_IP="${MINIKUBE_IP%.*}.$((${MINIKUBE_IP##*.}+1))"
+TO_IP="${MINIKUBE_IP%.*}.$((${MINIKUBE_IP##*.}+10))"
+
+# make sure we are in the correct context
+kubectl config use minikube
+
+# We have to congiure the loadbalancer IPs manually because of https://github.com/kubernetes/minikube/issues/8283
+# We configure the IPs for metallb loadbalancer in minikubes config.json and then start metallb addon. It will read the config and use the IPs for the metallb secret `config`.
+# This way we can avoid interactive command `minikube addons configure metallb`, and the ips are persistent.
+cat ~/.minikube/profiles/minikube/config.json | jq ".KubernetesConfig.LoadBalancerStartIP=\"$FROM_IP\"" | jq ".KubernetesConfig.LoadBalancerEndIP=\"$TO_IP\"" > ~/.minikube/profiles/minikube/config.json.tmp && mv ~/.minikube/profiles/minikube/config.json.tmp ~/.minikube/profiles/minikube/config.json 
+
+minikube addons enable metallb
+minikube addons enable ingress
+# minikube addons enable metrics-server
+
+_files=$(find  $_directory  | grep 'values')
+
+# make sure Treafik loadbalancer IP is correct in all configs
+for file in $_files
+do
+    while read -r _line; do
+    if [ "$_line" != "" ]; then
+      # replace loadbalancer ip's with actual ip's from minikube (traefik should always use the first loadbalancer ip)
+      sed -E -i "s/([.]*:\s)'([a-z-]*){0,1}([0-9]{1,3}\.){3}[0-9]{1,3}.nip.io'/\1'\2${FROM_IP}.nip.io'/g" $file
+    fi
+  done <<< $(cat $file | grep 'loadbalancer-IP')  
+done
+
+echo "Minikube Loadbalancer can be accessed with ${FROM_IP}.nip.io"
+
